fe33ca0c760bb884527a4c727bfd1a6b8412243c
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-03-09 07:50:16 +0900

    STATUS修正

--- lib/todomng/task.rb
+++ lib/todomng/task.rb
@@ -6,12 +6,12 @@ module TodoMng
   class Task < ActiveRecord::Base
 
     NOT_YET = 0 # タスクが完了していない
-    DONE = 1 #タスク完了
+    DONE    = 1 #タスク完了
     PENDING = 2 # 保留状態のタスク
 
     STATUS = {
-      'NOT_YET' => NOT_YET
-      'DONE' => DONE
+      'NOT_YET' => NOT_YET,
+      'DONE' => DONE,
       'PENDING' => PENDING
     }.freeze
   end
