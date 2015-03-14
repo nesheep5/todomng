03f3cf86f3e6fe6720f1184edf2df3d2441d034b
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-03-14 13:40:03 +0900

    ドキュメントコメント追加

--- lib/todomng/command.rb
+++ lib/todomng/command.rb
@@ -8,19 +8,29 @@ module TodoMng
     end
 
     # タスク作成
+    # @param [String] name 名前
+    # @param [String] content タスク内容
+    # @return [TodoMng::Task] タスクオブジェクト 
     def create_task(name, content)
       # タスク作成時のstatusはデフォルト値が使われNOT_YETとなる
-      # Win環境などコマンドラインとプログラムで扱うエンコードが異なる場合を考慮しreload
+      # Win環境などコマンドラインとプログラムでエンコードが異なる場合を考慮しreload
       Task.create!(name: name, content: content).reload
     end
     
     # タスク削除
+    # @param [String] id
     def delete_task(id)
       task = Task.find(id)
       task.destroy
     end
 
     # タスク更新
+    # @param [String] id
+    # @param [Hash] attributes
+    # @option attributes[String] :name 
+    # @option attributes[String] :content
+    # @option attributes[String] :status
+    # @return [TodoMng::Task] タスクオブジェクト 
     def update_task(id, attributes)
       if status_name = attributes[:status]
         attributes[:status] = Task::STATUS.fetch(status_name.upcase)
@@ -34,6 +44,8 @@ module TodoMng
     end
 
     # タスク検索
+    # @param [String] status_name
+    # @return [ActiveRecord::Relation] 検索結果
     def find_task(status_name)
       all_tasks = Task.order('created_at DESC')
 
