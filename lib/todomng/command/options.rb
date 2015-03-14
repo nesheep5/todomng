af7efaffd836ef2e5b57df0d9f4c1c70d7ef3bcb
Shogo Watanabe <ne.sheep.5mouths@gmail.com>
2015-03-15 00:57:46 +0900

    サブコマンド解析を追加

--- lib/todomng/command/options.rb
+++ lib/todomng/command/options.rb
@@ -6,6 +6,21 @@ module TodoMng
     module Options
 
       def self.parse!(argv)
+
+        options = {}
+
+        # サブコマンドの処理をする前に、未定義のkeyを指定されたら例外を発生させる
+        sub_command_parsers = Hash.new do |k, v|
+          raise ArgumentError, "'#{v}' is not todomng sub command."
+        end
+
+        # サブコマンド用の定義
+        sub_command_parsers['create'] = OptionParser.new do |opt|
+          opt.on('-n VAL', '--name=VAL',    'task name')    { |v| options[:name]    = v }
+          opt.on('-c VAL', '--content=VAL', 'task content') { |v| options[:content] = v }
+        end
+
+        # サブコマンド以外の引数の定義
         command_parser = OptionParser.new do |opt|
           opt.on_head('-v', '--version', 'Show program version') do |v|
             opt.version = TodoMng::VERSION
@@ -13,12 +28,21 @@ module TodoMng
             exit
           end
         end
-        
-        command_parser.parse!(argv)
+
+        # 引数の解析を行う
+        begin
+          command_parser.order!(argv)
+
+          options[:command] = argv.shift
+
+          sub_command_parsers[options[:command]].parse!(argv)
+
+          rescue OptionParser::MissingArgment, OptionParser::InvalidOption, ArgumentError => e
+          abort e.message
+        end
+
+        options
       end
-
     end
-
   end
-
 end
